f9599d1eb79848f6f75ff15052f3c23e16aaa1b1
Laboratorio <laboratorio@lab.unigran>
2022-09-23 19:56:40 -0400

    Banco de Dados criado

--- app/src/main/java/br/unigran/controledeabastecimento/BancoDados/DBHelper.java
+++ app/src/main/java/br/unigran/controledeabastecimento/BancoDados/DBHelper.java
@@ -1,4 +1,28 @@
 package br.unigran.controledeabastecimento.BancoDados;
 
-public class DBHelper  {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) { super(context,
+            "BancoLista",
+            null, 1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table Lista(" +
+                "id interger primary key autoincrement," +
+                "kmAtual varchar(7)," +
+                "qntAbastecido varchar(7)," +
+                "diaAbastecido varchar(7)," +
+                "valorAbastecido varchar(15))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/br/unigran/controledeabastecimento/BancoDados/RegistroDB.java
+++ app/src/main/java/br/unigran/controledeabastecimento/BancoDados/RegistroDB.java
@@ -1,5 +1,61 @@
 package br.unigran.controledeabastecimento.BancoDados;
 
-public class RegistroDB {
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import java.util.List;
+
+import br.unigran.controledeabastecimento.Registro;
+
+public class RegistroDB {
+    private DBHelper db;
+    private SQLiteDatabase conexao;
+
+    public RegistroDB(DBHelper db) { this.db = db; }
+
+    public void inserir(Registro registro) {
+        conexao = db.getWritableDatabase();//abre o bd
+        ContentValues valores = new ContentValues();
+        valores.put("campoNom", registro.getCampoNom());
+        valores.put("campoTel", registro.getCampoTel());
+        valores.put("campoDataNasc", registro.getCampoDataNasc());
+        conexao.update("Lista", valores, "id=?", new String[]{registro.getId().toString()});
+        conexao.close();
+    }
+
+    public void atualizar(Registro registro) {
+        conexao = db.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+        valores.put("campoNom", registro.getCampoNom());
+        valores.put("campoTel", registro.getCampoTel());
+        valores.put("campoDataNasc", registro.getCampoDataNasc());
+        conexao.update("Lista", valores, "id=?", new String[]{registro.getId().toString()});
+        conexao.close();
+    }
+
+    public void remover(int id){
+        conexao = db.getWritableDatabase();
+        conexao.delete("Lista","id=?", new String[]{id + ""});
+    }
+
+    public void lista(List dados){
+        dados.clear();
+        conexao = db.getReadableDatabase();
+        String names[] = {"id", "campoNom", "campoTel", "campoDataNasc"};
+        Cursor query = conexao.query("Lista", names, null, null, null, null, "campoNom");
+        while (query.moveToNext()){
+            Registro registro = new Registro();
+            registro.setId(Integer.parseInt(
+                    query.getString(0)));
+            registro.setCampoNom(
+                    query.getString(1));
+            registro.setCampoTel(
+                    query.getString(2));
+            registro.setCampoDataNasc(
+                    query.getString(3));
+            dados.add(registro);
+        }
+        conexao.close();
+    }
 }
